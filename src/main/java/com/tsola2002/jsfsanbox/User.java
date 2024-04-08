490fa2ec6019d0ad01cfd8c32fab1666e06ba5f9
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-04-08 10:52:21 +0100

    Initial Commit

---
+++ src/main/java/com/tsola2002/jsfsanbox/User.java
@@ -0,0 +1,28 @@
+package com.tsola2002.jsfsanbox;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean(name="user")
+public class User {
+	
+	private String username, password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
