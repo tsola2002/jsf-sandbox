76556f7d08f7ca19f6cf3c7dfb51158fc3d5bdcb
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-08-10 11:31:32 +0100

    fixed issues

--- 01-INTRODUCTION/untitled/src/main/java/com/tsola2002/bean/StudentBean.java
+++ 01-INTRODUCTION/untitled/src/main/java/com/tsola2002/bean/StudentBean.java
@@ -3,70 +3,59 @@ package com.tsola2002.bean;
 import com.tsola2002.entity.Student;
 import com.tsola2002.service.StudentService;
 
-
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.ViewScoped;
 import java.io.Serializable;
 import java.util.List;
 
 @ManagedBean(name = "studentBean")
 @ViewScoped
 public class StudentBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private Student student = new Student();
 
     private List<Student> students;
 
     private StudentService service = new StudentService();
 
-
+    @PostConstruct
     public void init() {
-
         students = service.list();
-
     }
 
     public void save() {
 
-        if (student.getId() == null)
+        if (student.getId() == null) {
             service.save(student);
-        else
+        } else {
             service.update(student);
+        }
 
         student = new Student();
-
         students = service.list();
-
     }
 
     public void edit(Student s) {
-
         student = s;
-
     }
 
     public void delete(Student s) {
-
         service.delete(s);
-
         students = service.list();
-
     }
 
     public Student getStudent() {
-
         return student;
-
     }
 
     public void setStudent(Student student) {
-
         this.student = student;
-
     }
 
     public List<Student> getStudents() {
-
         return students;
-
     }
 }
